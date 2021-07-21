e43d4ba158eefe41411af3657a7123d035b54140
Christopher Aust <christopher.aust@student.hpi.uni-potsdam.de>
2021-07-21 20:47:49 +0200

    add script embedding python, Task 6.3

--- script-embedding-python/main.cpp
+++ script-embedding-python/main.cpp
@@ -1,13 +1,35 @@
-
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-	for (auto i = 0; i <= 100; ++i)
-	{
-        const auto result = 0; // TODO: compute with process(i) from process.py
-		std::cout << "Call with " << i << " resulted in " << result << '\n';
-		
-	}
+    Py_Initialize();
+
+    PyObject * sys = PyImport_ImportModule("sys");
+    PyObject * path = PyObject_GetAttrString(sys, "path");
+    PyList_Append(path, PyUnicode_FromString("../"));
+
+    PyObject * pName = PyUnicode_DecodeFSDefault("process");
+    PyObject  * pModule = PyImport_Import(pName);
+    PyObject * pFunc = PyObject_GetAttrString(pModule, "process");
+
+    Py_DecRef(pName);
+    Py_DecRef(pModule);
+
+    if (pFunc &&PyCallable_Check(pFunc)) {
+
+        for (auto i = 0; i <= 100; ++i) {
+            PyObject * pArgs = PyTuple_New(1);
+            PyObject * pValue = PyLong_FromLong(i);
+            PyTuple_SetItem(pArgs, 0, pValue);
+            const auto pResult = PyObject_CallObject(pFunc, pArgs);
+            const auto result = PyLong_AsLong(pResult);
+            std::cout << "Call with " << i << " resulted in " << result << '\n';
+
+            Py_DecRef(pResult);
+
+        }
+    }
 	
-	return 0;
+	return Py_FinalizeEx() < 0 ? -1 : 0;
 }
